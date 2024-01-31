32af55747e263861106c91291226a55076f181f5
Arun Kumar R <aruntutter19@gmail.com>
2024-01-31 20:16:06 +0530

    Locomotive imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Page1 from "./components/Page1";
 
+import LocomotiveScroll from "locomotive-scroll";
+const locomotiveScroll = new LocomotiveScroll();
+
 const App = () => {
   return (
     <div>
