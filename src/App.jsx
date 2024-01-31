54442a94e27033c575ba8ae6ac62559e18e667bb
Arun Kumar R <aruntutter19@gmail.com>
2024-01-31 20:04:08 +0530

    page created and imported 3 times

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Page1 from "./components/Page1";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Page1 />
+      <Page1 />
+      <Page1 />
+    </div>
+  );
 };
 
 export default App;
